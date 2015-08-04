323d719237098e4392adc047feb973f4e77c56c8
Eugene Narciso <eugene.narciso@itaas.dimensiondata.com>
2015-08-04 12:43:35 -0700

    bumping version to 1.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'michael.m.morris@gmail.com'
 license '3-clause BSD'
 description 'Installs/Configures Yum repository server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.0.2'
 
 %w(redhat centos).each do |p|
   supports p
